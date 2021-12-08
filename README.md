9cad5d5b4972ca34614aa5268c4e07a17762b02d
Jovian <99047257@mydavinci.nl>
2021-12-08 13:07:47 +0100

    Update readme + opdrachten

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Tkinter
 ## F1.7.01.O1
 Repo aangemaakt
+## F1.7.01.O2
+Lichtschakelaar gemaakt
+```python
+def buttonPress(event):
+    if button.config('text')[-1] == 'Switch light on':
+        button.config(text='Switch light off',bg="white",fg="black")
+        window.config(bg="black")  
+        print("light is off")    
+    else:
+        button.config(text='Switch light on',bg="white",fg="blue")
+        window.config(bg="yellow")      
+        print("light is on")
+button.bind('<Button>', buttonPress)
+```
--- lightswitch.py
+++ lightswitch.py
@@ -1,13 +1,20 @@
 import tkinter as tk
 window = tk.Tk()
-
-button = tk.Button(text='...', bg="white", fg="black")
-button.pack(pady = 20, padx = 20)
+window.config(bg="black")
+button = tk.Button(text='Switch light off', bg="white", fg="black")
+button.pack(pady = 50, padx = 100)
 
 # schijf hier tussen je code
-
-
-
+def buttonPress(event):
+    if button.config('text')[-1] == 'Switch light on':
+        button.config(text='Switch light off',bg="white",fg="black")
+        window.config(bg="black")  
+        print("light is off")    
+    else:
+        button.config(text='Switch light on',bg="white",fg="blue")
+        window.config(bg="yellow")      
+        print("light is on")
+button.bind('<Button>', buttonPress)
 # schijf hier tussen je code
 
 window.mainloop()
